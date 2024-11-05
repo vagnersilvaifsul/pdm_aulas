e2c2f956caf8a1341cc3ce04e3782c93383cc126
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2024-11-05 11:53:57 -0300

    feat: modulo 1, signIn, form com estilos

--- src/telas/SignIn.tsx
+++ src/telas/SignIn.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, ScrollView, StyleSheet} from 'react-native';
+import {Image, ScrollView, StyleSheet, View} from 'react-native';
 import {Button, Text, TextInput} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
@@ -8,12 +8,26 @@ export default function SignIn({navigation}: any) {
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <>
-          <Image source={require('../assets/logo512.png')} />
-          <TextInput label="Email" mode="outlined" placeholder="Digite seu email" />
-          <TextInput label="senha" mode="outlined" placeholder="Digite sua senha" />
-          <Text>Esqueceu sua senha?</Text>
-          <Button onPress={() => navigation.navigate('Home')}>Engtrar</Button>
-          <Text>Cadastrar-se?</Text>
+          <Image style={styles.image} source={require('../assets/logo512.png')} />
+          <TextInput
+            style={styles.textinput}
+            label="Email"
+            mode="outlined"
+            placeholder="Digite seu email"
+          />
+          <TextInput
+            style={styles.textinput}
+            label="senha"
+            mode="outlined"
+            placeholder="Digite sua senha"
+          />
+          <Text style={styles.textEsqueceuSenha}>Esqueceu sua senha?</Text>
+          <Button style={styles.button} onPress={() => navigation.navigate('Home')}>
+            Engtrar
+          </Button>
+          <View style={styles.divCadastro}>
+            <Text style={styles.textCadastro}>Cadastrar-se?</Text>
+          </View>
         </>
       </ScrollView>
     </SafeAreaView>
@@ -25,8 +39,31 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
-  texto: {
-    color: 'red',
-    fontSize: 30,
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    borderRadius: 200 / 2,
+    marginTop: 100,
+    marginBottom: 40,
+  },
+  textinput: {
+    width: 350,
+    height: 50,
+    marginBottom: 20,
+    backgroundColor: 'transparent',
+  },
+  textEsqueceuSenha: {
+    alignSelf: 'flex-end',
+  },
+  textCadastro: {},
+  button: {
+    marginTop: 50,
+    marginBottom: 30,
+  },
+  divCadastro: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
 });
