efd98db2f7a57b994f927dec2e2c9cba82d4c6d5
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-09-03 11:31:44 -0300

    feat: entrar fonçando o login com literais

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,31 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect } from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function SignIn() {
+	useEffect(() => {
+		entrar();
+	}, []);
+
+	async function entrar() {
+		console.log("Chamou entrar");
+		const auth = getAuth();
+		try {
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				"teste@email.com",
+				"Teste123"
+			);
+			console.log(userCredential.user);
+			console.log(userCredential.user.email);
+			//router.replace("/(tabs)");
+		} catch (error: any) {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log(errorCode, errorMessage);
+		}
+	}
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<Text style={styles.texto}>Sign In</Text>
