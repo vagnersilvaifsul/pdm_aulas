59c93cd4a7dce4b86414ba7c13704531f4032d33
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-09-24 10:16:47 -0300

    feat: entrar, tratamento de mensagens de erro de autenticação

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -17,10 +17,27 @@ export const AuthProvider = ({ children }: any) => {
 			console.log(userCredential.user.email);
 			return "ok";
 		} catch (error: any) {
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			console.log(errorCode, errorMessage);
-			return "erro";
+			return launchServerMessageErro(error);
+		}
+	}
+
+	//função utilitária
+	function launchServerMessageErro(e: any): string {
+		switch (e.code) {
+			case "auth/invalid-credential":
+				return "Email inexistente ou senha errada.";
+			case "auth/user-not-found":
+				return "Usuário não cadastrado.";
+			case "auth/wrong-password":
+				return "Erro na senha.";
+			case "auth/invalid-email":
+				return "Email inexistente.";
+			case "auth/user-disabled":
+				return "Usuário desabilitado.";
+			case "auth/email-already-in-use":
+				return "Email em uso. Tente outro email.";
+			default:
+				return "Erro desconhecido. Contate o administrador.";
 		}
 	}
 
