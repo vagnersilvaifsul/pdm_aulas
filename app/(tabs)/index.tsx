5906f922395c3fc1fa359ec348ff6f61ec210a66
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-02-27 20:54:21 -0300

    primeiro commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+
+
+//Uma função para ter o componente react
+
+import {Text, View} from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View><Text>Home Screen</Text></View>
+	)
+}
