eabdaac1f8398b7660607b7f910a96ae481e5fd2
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-03-05 14:08:46 -0300

    semana2: fundamentos de react

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
 
-
-//Uma função para ter o componente react
-
-import {Text, View} from "react-native";
-
-export default function HomeScreen() {
+export function MeuBotao(props: any) {
+	console.log("Meu Botão", props);
 	return (
-		<View><Text>Home Screen</Text></View>
-	)
+		<TouchableHighlight onPress={props.onClick} underlayColor="lightgray">
+			<Text style={styles.text}>{props.texto}</Text>
+		</TouchableHighlight>
+	);
 }
+
+/*
+ Uma função só é considerada um componente React se retornar um JSX.
+ */
+export default function HomeScreen() {
+	const [count, setCount] = useState(0);
+
+	console.log("Home Screen");
+
+	/*
+	 Um componente React deve retornar um JSX.
+	*/
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Home Screen {count}</Text>
+			<MeuBotao texto="Increment" onClick={() => setCount(count + 1)} />
+			<MeuBotao texto="Decrement" onClick={() => setCount(count - 1)} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		fontSize: 50,
+		fontWeight: "bold",
+	},
+});
