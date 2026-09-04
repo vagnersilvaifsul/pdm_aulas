887f417ce9f10bb57f96924bc4b0e11c5aa393ed
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-09-04 10:10:39 -0300

    feat: modulo1_sair tela menu

--- src/app/(tabs)/menu.tsx
+++ src/app/(tabs)/menu.tsx
@@ -1,10 +1,70 @@
-import { StyleSheet, Text } from "react-native";
+// eslint-disable-next-line import/no-unresolved
+import { AuthContext } from "@/context/AuthProvider";
+import { router } from "expo-router";
+import { useContext, useState } from "react";
+import { StyleSheet } from "react-native";
+import { Dialog, Divider, List, Text, useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Menu() {
+	const theme = useTheme();
+	const { sair } = useContext<any>(AuthContext);
+	const [dialogVisivel, setDialogVisivel] = useState(false);
+
+	async function handleSair() {
+		if (await sair()) {
+			router.replace("/entrar"); //Veja a branch modulo1_preload para ver como isso está funcionando
+		} else {
+			setDialogVisivel(true);
+		}
+	}
+
 	return (
-		<SafeAreaView style={styles.container}>
-			<Text style={styles.text}>Menu</Text>
+		<SafeAreaView
+			style={{ ...styles.container, backgroundColor: theme.colors.background }}
+		>
+			<List.Item
+				title="Perfil"
+				description="Atualize seu perfil ou exclua sua conta"
+				left={() => (
+					<List.Icon color={theme.colors.primary} icon="smart-card-outline" />
+				)}
+				onPress={() => alert("em desenvolvimento")}
+			/>
+			<Divider />
+			<List.Item
+				title="Alterar Senha"
+				description="Altere sua senha"
+				left={() => (
+					<List.Icon
+						color={theme.colors.primary}
+						icon="eye-arrow-right-outline"
+					/>
+				)}
+				onPress={() => alert("em desenvolvimento")}
+			/>
+			<Divider />
+			<List.Item
+				title="Sair"
+				description="Finaliza sua sessão no aplicativo"
+				left={() => <List.Icon color={theme.colors.primary} icon="exit-run" />}
+				onPress={handleSair}
+			/>
+			<Dialog
+				visible={dialogVisivel}
+				onDismiss={() => {
+					setDialogVisivel(false);
+				}}
+			>
+				<Dialog.Icon icon={"alert-circle-outline"} size={60} />
+				<Dialog.Title style={styles.textDialog}>'Ops!'</Dialog.Title>
+				<Dialog.Content>
+					<Text style={styles.textDialog} variant="bodyLarge">
+						{`Estamos com problemas para realizar essa operação.\nPor favor,
+            contate o administrador.`}
+					</Text>
+				</Dialog.Content>
+			</Dialog>
 		</SafeAreaView>
 	);
 }
@@ -12,11 +72,11 @@ export default function Menu() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		paddingLeft: 20,
+		paddingTop: 20,
 		alignItems: "center",
-		justifyContent: "center",
 	},
-	text: {
-		fontSize: 20,
-		fontWeight: "bold",
+	textDialog: {
+		textAlign: "center",
 	},
 });
